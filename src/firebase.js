1905044dbdd60e68943dc7ae4a7879323c5abf6b
Ali H. Shah <sshah37@luc.edu>
2017-04-06 18:49:25 -0500

    firebase implementation added

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,13 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: "AIzaSyDO9Fnyf1YOwsZJ46RshxAEYbZJLNa-19w",
-  authDomain: "goalcoach-6f23e.firebaseapp.com",
-  databaseURL: "https://goalcoach-6f23e.firebaseio.com",
-  storageBucket: "goalcoach-6f23e.appspot.com",
-  messagingSenderId: "568769933089"
-};
+    apiKey: "AIzaSyAwazsynIViv5rTDka8alvjeUY7QLnJDOg",
+    authDomain: "webtask-5e801.firebaseapp.com",
+    databaseURL: "https://webtask-5e801.firebaseio.com",
+    projectId: "webtask-5e801",
+    storageBucket: "webtask-5e801.appspot.com",
+    messagingSenderId: "630366854641"
+  };
 
 export const firebaseApp = firebase.initializeApp(config);
 export const goalRef = firebase.database().ref('goals');
